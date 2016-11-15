f3da8d97b902c2eb6cf32af537937668b563dc80
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-11-15 18:10:47 +0300

    i love cats

--- solar_vis.py
+++ solar_vis.py
@@ -34,7 +34,7 @@ def scale_x(x):
     В случае выхода **x** координаты за пределы экрана возвращает
     координату, лежащую за пределами холста.
 
-    Параметры:
+    Параметры:мяяяу
 
     **x** — x-координата модели.
     """
@@ -54,7 +54,7 @@ def scale_y(y):
     **y** — y-координата модели.
     """
 
-    return y  # FIXME: not done yet
+    return   int(y*scale_factor)+window_height//2
 
 
 def create_star_image(space, star):
@@ -78,10 +78,13 @@ def create_planet_image(space, planet):
     Параметры:
 
     **space** — холст для рисования.
-    **planet** — объект планеты.
-    """
-    pass  # FIXME: сделать как у звезды
+    **planet** — объект планеты."""
 
+    pass
+    x=scale_x(planet.x)
+    y=scale_y(planet.y)
+    r=planet.R
+    planet.image = space.create_oval([x - r, y - r], [x + r, y + r], fill=planet.color)
 
 def update_system_name(space, system_name):
     """Создаёт на холсте текст с названием системы небесных тел.
